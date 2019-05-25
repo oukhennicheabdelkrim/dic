c1b44845a70f3de067f6c0f8a7769e95727fe728
Abdelkrim OUKHENNICHE <oukhennicheabdelkrim@gmail.com>
2019-05-25 05:18:43 +0200

    add exceptions

---
+++ src/Definition/Exceptions/NotFoundException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kikim
+ * Date: 24/05/2019
+ * Time: 17:34
+ */
+
+namespace oukhennicheabdelkrim\DIC\Definition\Exceptions;
+
+
+use Psr\Container\NotFoundExceptionInterface;
+
+
+class NotFoundException extends \Exception implements NotFoundExceptionInterface
+{
+
+}
---
+++ src/Definition/Exceptions/NotInstantiableExecption.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kikim
+ * Date: 24/05/2019
+ * Time: 19:40
+ */
+
+namespace oukhennicheabdelkrim\DIC\Definition\Exceptions;
+
+
+use Psr\Container\ContainerExceptionInterface;
+
+
+class NotInstantiableExecption extends \Exception implements ContainerExceptionInterface
+{
+
+}
