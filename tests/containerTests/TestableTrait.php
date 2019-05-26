d160378bbf2e5939105270dbe72b6eb2e2141050
Abdelkrim OUKHENNICHE <oukhennicheabdelkrim@gmail.com>
2019-05-26 22:48:26 +0200

    update containerTests/TestableTrait

--- tests/containerTests/TestableTrait.php
+++ tests/containerTests/TestableTrait.php
@@ -4,7 +4,7 @@ namespace oukhennicheabdelkrim\DIC\tests\containerTests;
 
 use oukhennicheabdelkrim\DIC\DIC;
 
-require_once dirname(__DIR__).'/TestClass/bootstrap.php';
+
 
 
 trait TestableTrait
@@ -16,8 +16,15 @@ trait TestableTrait
 
     public $container;
 
+    public function __construct()
+    {
+        parent::__construct();
+        require_once dirname(__DIR__).'/TestClass/bootstrap.php';
+    }
+
     protected function setUp()
     {
+        parent::setUp();
         $this->container=new DIC();
     }
 }
